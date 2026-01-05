19933a5cb7accd55b4d1e4b908e12fa1c43749cb
cayurid <cayurid@gmail.com>
2026-01-05 17:43:09 -0300

    muita coisa

--- ConsoleApp2/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ ConsoleApp2/WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfab3979622921404f5ac0848b4fefc61b08e1f5")]
-[assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp2")]
+[assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
